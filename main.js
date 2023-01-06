a77b03385fb575966614ea3052c93efb4b25b7c8
jessvoiture <jessicascarr989@gmail.com>
2023-01-05 21:57:13 -0500

    update

--- main.js
+++ main.js
@@ -1,17 +1,30 @@
+// side panel
+$('.sp-clicker').click(function(e) {
+
+  // show side panel
+  $('.sidepanel').toggleClass("show-sp"); 
+  $('.sp-clicker').toggleClass("move-clicker"); 
+  // stop scrolling when side panel is visible
+  $('body').toggleClass("no-scroll"); 
+  e.preventDefault();
+
+});
+
+// hover to show image
 let attached = false;
 
 var imgHeight;
 
 const getElmtImage = (elmt) => {
-  return elmt.querySelector("img")
+  return elmt.querySelector(".hover-image")
 }
 
 const getImageHeight = (elmt) => {
-  return elmt.querySelector("img").height;
+  return elmt.querySelector(".hover-image").height;
 }
 
 const followMouse = (elmt, event, height) => {
-  elmt.style.left = event.x + "px";
+  elmt.style.left = (event.x + 100) + "px";
   elmt.style.top = (event.y - height) + "px";
 }
 
